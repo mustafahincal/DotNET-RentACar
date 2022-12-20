01d4e931b4710de377b3ee3ecd2aaa1d2c1d4087
mustafahincal <mustafahncal@gmail.com>
2022-12-20 19:52:38 +0300

    update connection string and add migration

--- DataAccess/Concrete/EntityFramework/CarRentalContext.cs
+++ DataAccess/Concrete/EntityFramework/CarRentalContext.cs
@@ -7,25 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Concrete.EntityFramework {
-    public class CarRentalContext : DbContext {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-QJHUFHB;Database=CarRental;Trusted_Connection=True");
-        }
+namespace DataAccess.Concrete.EntityFramework
+{
+      public class CarRentalContext : DbContext
+      {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                  optionsBuilder.UseSqlServer(@"Server=localhost;Database=CarRental;User Id=SA;Password=MyPassword123#;MultipleActiveResultSets=True;");
 
-        public DbSet<Car> Cars { get; set; }
-        public DbSet<Brand> Brands { get; set; }
-        public DbSet<Color> Colors { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Rental> Rentals { get; set; }
-        public DbSet<CarImage> CarImages { get; set; }
+            }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<OperationClaim> OperationClaims { get; set; }
-        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+            public DbSet<Car> Cars { get; set; }
+            public DbSet<Brand> Brands { get; set; }
+            public DbSet<Color> Colors { get; set; }
+            public DbSet<Customer> Customers { get; set; }
+            public DbSet<Rental> Rentals { get; set; }
+            public DbSet<CarImage> CarImages { get; set; }
 
-        public DbSet<CreditCard> CreditCards { get; set; }
+            public DbSet<User> Users { get; set; }
+            public DbSet<OperationClaim> OperationClaims { get; set; }
+            public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
 
-        public DbSet<Model> Models { get; set; }
-    }
+            public DbSet<CreditCard> CreditCards { get; set; }
+
+            public DbSet<Model> Models { get; set; }
+      }
 }
