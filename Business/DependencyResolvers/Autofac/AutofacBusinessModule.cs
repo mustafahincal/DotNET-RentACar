46d4a4249517b152bce4abf0adbf118a7953e854
mustafahincal <mustafahncal@gmail.com>
2022-12-26 11:04:29 +0300

    reset database

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -16,41 +16,44 @@ using System.Threading.Tasks;
 
 namespace Business.DependencyResolvers.Autofac
 {
-    public class AutofacBusinessModule : Module
-    {
-        protected override void Load(ContainerBuilder builder) {
-            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
-            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
-            builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
-            builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
-            builder.RegisterType<ModelManager>().As<IModelService>().SingleInstance();
-            builder.RegisterType<EfModelDal>().As<IModelDal>().SingleInstance();
-            builder.RegisterType<ColorManager>().As<IColorService>().SingleInstance();
-            builder.RegisterType<EfColorDal>().As<IColorDal>().SingleInstance();
-            builder.RegisterType<CustomerManager>().As<ICustomerService>().SingleInstance();
-            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().SingleInstance();
-            builder.RegisterType<RentalManager>().As<IRentalService>().SingleInstance();
-            builder.RegisterType<EfRentalDal>().As<IRentalDal>().SingleInstance();
-            builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
-            builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
-            builder.RegisterType<CarImageManager>().As<ICarImageService>().SingleInstance();
-            builder.RegisterType<EfCarImageDal>().As<ICarImageDal>().SingleInstance();
-            builder.RegisterType<FileHelper>().As<IFileHelper>().SingleInstance();
+      public class AutofacBusinessModule : Module
+      {
+            protected override void Load(ContainerBuilder builder)
+            {
+                  builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+                  builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+                  builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
+                  builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
+                  builder.RegisterType<ModelManager>().As<IModelService>().SingleInstance();
+                  builder.RegisterType<EfModelDal>().As<IModelDal>().SingleInstance();
+                  builder.RegisterType<ColorManager>().As<IColorService>().SingleInstance();
+                  builder.RegisterType<EfColorDal>().As<IColorDal>().SingleInstance();
+                  builder.RegisterType<CustomerManager>().As<ICustomerService>().SingleInstance();
+                  builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().SingleInstance();
+                  builder.RegisterType<RentalManager>().As<IRentalService>().SingleInstance();
+                  builder.RegisterType<EfRentalDal>().As<IRentalDal>().SingleInstance();
+                  builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
+                  builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
+                  builder.RegisterType<CarImageManager>().As<ICarImageService>().SingleInstance();
+                  builder.RegisterType<EfCarImageDal>().As<ICarImageDal>().SingleInstance();
+                  builder.RegisterType<FileHelper>().As<IFileHelper>().SingleInstance();
 
-            builder.RegisterType<AuthManager>().As<IAuthService>();
-            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+                  builder.RegisterType<AuthManager>().As<IAuthService>();
+                  builder.RegisterType<JwtHelper>().As<ITokenHelper>();
 
-            builder.RegisterType<CreditCardManager>().As<ICreditCardService>().SingleInstance();
-            builder.RegisterType<EfCreditCardDal>().As<ICreditCardDal>().SingleInstance();
+                  builder.RegisterType<EfUserOperationClaimDal>().As<IUserOperationClaimDal>().SingleInstance();
+
+                  builder.RegisterType<CreditCardManager>().As<ICreditCardService>().SingleInstance();
+                  builder.RegisterType<EfCreditCardDal>().As<ICreditCardDal>().SingleInstance();
 
 
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                  var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
-            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
-                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
-                {
-                    Selector = new AspectInterceptorSelector()
-                }).SingleInstance();
-        }
-    }
+                  builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                      .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                      {
+                            Selector = new AspectInterceptorSelector()
+                      }).SingleInstance();
+            }
+      }
 }
